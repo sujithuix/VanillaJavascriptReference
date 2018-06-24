e3eaec357bf73006c1666d87d580cf1858a75fe4
SJ Sujith <sujith777@gmail.com>
2018-06-24 18:28:08 +0530

    intial commit of app source

---
+++ src/app/javascript/javascript.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import * as Chartist from 'chartist';
+
+@Component({
+  selector: 'app-javascript',
+  templateUrl: './javascript.component.html',
+  styleUrls: ['./javascript.component.css']
+})
+export class JavascriptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
